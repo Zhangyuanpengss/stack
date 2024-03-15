87128e136b2b7e6518c3aa9bfb8aec0a66e7c1aa
Zhangyuanpengss <161277716+Zhangyuanpengss@users.noreply.github.com>
2024-03-15 14:05:13 +0800

    Add files via upload

---
+++ Stack/Stack/src/BalancedParentheses.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class BalancedParentheses{
+    Stack<Character> stack = new Stack<>();
+
+    public void balancedParentheses(String expression){
+        for(char bracket: expression.toCharArray()){
+            if(bracket == '{' || bracket =='(' || bracket == '['){
+                stack.push(bracket);
+            }
+            else if (bracket == '}' || bracket ==')' || bracket == ']'){
+                if(stack.isEmpty()  || !isItBalanced(stack.pop(),bracket)){
+                   System.out.println("Not balanced");
+                   return;}
+            }
+        }
+        System.out.println("balance");}      
+    public boolean isItBalanced(char open,char close){
+        if(open == '{' && close == '}'){
+            return true;
+        }else if (open == '(' && close == ')'){
+            return true;
+        }else if (open == '[' && close == ']'){
+            return true;
+        }
+        return false;
+}
+}
